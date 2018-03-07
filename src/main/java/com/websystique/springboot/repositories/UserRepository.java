eb54f0a5cb52525171d8ecdb8a74c1ac987a97ac
yonatan mengesha <mail2yonz@gmail.com>
2018-03-07 13:14:47 -0500

    Initial commit

---
+++ src/main/java/com/websystique/springboot/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.websystique.springboot.repositories;
+
+import com.websystique.springboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
+
+}
